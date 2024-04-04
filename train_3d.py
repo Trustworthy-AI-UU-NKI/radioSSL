c959d1ee97ff1acbe9abfa0e3db2b5e57f5ebc92
joangog <ioannagogou1998@gmail.com>
2024-04-04 15:58:35 +0200

    Grid visualization implementation + lits  data loader progress

--- train_3d.py
+++ train_3d.py
@@ -5,21 +5,26 @@ Training code for C2L
 from __future__ import print_function
 
 import os
+import io
 import sys
 import time
 import math
+import random
+import PIL
+
 import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.nn.functional as f
-import random
-from utils import adjust_learning_rate, AverageMeter, sinkhorn, swav_loss, roi_align_intersect
+import torchvision.transforms.functional as t
 from torch.utils.tensorboard import SummaryWriter
 
 import seaborn as sns
 from matplotlib import pyplot as plt
 
 from models import PCRLv23d, Cluster3d, TraceWrapper
+from utils import adjust_learning_rate, AverageMeter, sinkhorn, swav_loss, roi_align_intersect
+
 
 try:
     from apex import amp, optimizers
@@ -51,7 +56,7 @@ def cos_loss(cosine, output1, output2):
 def train_3d(args, data_loader, run_dir, out_channel=3, writer=None):
 
     if args.model == 'cluster':
-        # Generate colors for cluster masks (make 10 of them, but later TODO: base it on cluster number K)
+        # Generate colors for cluster masks
         palette = sns.color_palette(palette='bright', n_colors=args.k)
         colors = torch.Tensor([list(color) for color in palette])
 
@@ -83,7 +88,11 @@ def train_3d(args, data_loader, run_dir, out_channel=3, writer=None):
         cosine = cosine.cuda()
         cudnn.benchmark = True
 
+    grid_preds = []  # Clustering task predictions to visualize in grid (epoch X image) 
+
     for epoch in range(0, args.epochs + 1):
+
+        # TRAINING
 
         adjust_learning_rate(epoch, args, optimizer)
         print("==> training...")
@@ -101,6 +110,12 @@ def train_3d(args, data_loader, run_dir, out_channel=3, writer=None):
         if args.tensorboard:
             writer.add_scalar('loss/train', total_loss, epoch)  # Write train loss on tensorboard
 
+        # VALIDATION (only for clustering task, just for visualization purposes)
+
+        if args.model == 'cluster' and args.n == 'brats':  # TODO: currently only works for BraTS dataset
+            row_preds = val_cluster_inner(args, epoch, val_loader, model, colors)
+            grid_preds.extend(row_preds)
+
         # Save model
         if epoch % 100 == 0 or epoch == 240:
             print('==> Saving...')
@@ -112,15 +127,34 @@ def train_3d(args, data_loader, run_dir, out_channel=3, writer=None):
 
             # Help release GPU memory
             del state
+
         if not args.cpu:
             torch.cuda.empty_cache()
+    
+    # Visualize grid of predictions for clustering task
+    if args.model == 'cluster' and args.n == 'brats':  # TODO: Currently only works for BraTS dataset
+        n_rows = min(10, args.epochs+1) + 1 # (+1 because we run for one extra epoch, and +1 for the first row which is the input images)
+        n_cols = min(10, args.b)
+        fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 15*(n_rows/n_cols)))
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(grid_preds[i])
+            ax.axis('off')  # Turn off axis labels
+        plt.tight_layout()  # Adjust spacing between subplots
+        
+        # Save grid to buffer and then log on tensorboard
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png', bbox_inches='tight', dpi=100)
+        buf.seek(0)
+        grid = PIL.Image.open(buf)
+        grid = t.pil_to_tensor(grid)
+        writer.add_image('img/val/grid', img_tensor=grid)
 
-
+        
 def train_pcrlv2_inner(args, epoch, train_loader, model, optimizer, criterion, cosine, writer):
     """
     one epoch training for instance discrimination
     """
-
+    
     model.train()
 
     batch_time = AverageMeter()
@@ -215,8 +249,6 @@ def train_pcrlv2_inner(args, epoch, train_loader, model, optimizer, criterion, c
                 data_time=data_time, c2l_loss=loss_meter, mg_loss=mg_loss_meter, prob=prob_meter))
             sys.stdout.flush()
 
-    # TODO: Add eval loop just for visualization
-
     return mg_loss_meter.avg, prob_meter.avg, total_loss_meter.avg, writer
 
 
@@ -225,9 +257,6 @@ def train_pcrlv2_3d(args, data_loader, run_dir, out_channel=3, writer=None):
 
 
 def train_cluster_inner(args, epoch, train_loader, model, optimizer, criterion, cosine, writer, colors):
-    """
-    one epoch training for instance discrimination
-    """
 
     model.train()
 
@@ -245,7 +274,6 @@ def train_cluster_inner(args, epoch, train_loader, model, optimizer, criterion, 
 
         data_time.update(time.time() - end)
 
-        bsz = input1.size(0)
         x1 = input1.float()  # Crop 1
         x2 = input2.float()  # Crop 2
 
@@ -309,7 +337,7 @@ def train_cluster_inner(args, epoch, train_loader, model, optimizer, criterion, 
         NPH = H//PH  # Num patches at X
         NPW = W//PW  # Num patches at Y
         NPD = D//PD  # Num patches at Z
-        pred1 = pred1.permute(0,2,1).reshape((B,K,NPH,NPW,NPD))  # TODO: Later, remove argmax and use soft clusters
+        pred1 = pred1.permute(0,2,1).reshape((B,K,NPH,NPW,NPD))
         pred2 = pred2.permute(0,2,1).reshape((B,K,NPH,NPW,NPD))
         gt1 = gt1.permute(0,2,1).reshape((B,K,NPH,NPW,NPD))
         gt2 = gt2.permute(0,2,1).reshape((B,K,NPH,NPW,NPD))
@@ -386,9 +414,9 @@ def train_cluster_inner(args, epoch, train_loader, model, optimizer, criterion, 
             pred_img = torch.cat((pred1,pred2),dim=3).squeeze(0).cpu().detach().numpy()
             gt_img = torch.cat((gt1,gt2),dim=3).squeeze(0).cpu().detach().numpy()
 
-            in_img_name = 'img/raw' # f'b{b_idx}_img{img_idx}_slc{slc_idx}_raw'
-            pred_img_name = 'img/pred' # f'b{b_idx}_img{img_idx}_slc{slc_idx}_pred'
-            gt_img_name = 'img/gt' # f'b{b_idx}_img{img_idx}_slc{slc_idx}_gt'
+            in_img_name = 'img/train/raw' # f'b{b_idx}_img{img_idx}_slc{slc_idx}_raw'
+            pred_img_name = 'img/train/pred' # f'b{b_idx}_img{img_idx}_slc{slc_idx}_pred'
+            gt_img_name = 'img/train/gt' # f'b{b_idx}_img{img_idx}_slc{slc_idx}_gt'
 
             writer.add_image(in_img_name, img_tensor=in_img, global_step=epoch, dataformats='CHW')
             writer.add_image(pred_img_name, img_tensor=pred_img, global_step=epoch, dataformats='CHW')   
@@ -413,10 +441,10 @@ def train_cluster_inner(args, epoch, train_loader, model, optimizer, criterion, 
         optimizer.step()
 
         # Meters
-        mg_loss_meter.update(loss1.item(), bsz)
-        loss_meter.update(loss2.item(), bsz)
-        prob_meter.update(local_loss, bsz)
-        total_loss_meter.update(loss, bsz)
+        mg_loss_meter.update(loss1.item(), B)
+        loss_meter.update(loss2.item(), B)
+        prob_meter.update(local_loss, B)
+        total_loss_meter.update(loss, B)
         if not args.cpu:
             torch.cuda.synchronize()
         batch_time.update(time.time() - end)
@@ -439,9 +467,73 @@ def train_cluster_inner(args, epoch, train_loader, model, optimizer, criterion, 
                 data_time=data_time, c2l_loss=loss_meter, mg_loss=mg_loss_meter, prob=prob_meter))
             sys.stdout.flush()
 
-    # TODO: Add eval loop just for visualization
-
     return mg_loss_meter.avg, prob_meter.avg, total_loss_meter.avg, writer
+
+
+def val_cluster_inner(args, epoch, val_loader, model, colors):
+
+    with torch.no_grad():
+
+        grid_preds = []  # For plotting a grid epoch X image: A row of predicted cluster assignments for each image in the current epoch
+
+        model.eval()
+
+        for idx, (image, _, gt, _, crop_coords, _, _) in enumerate(val_loader):
+            
+            if idx == 1:
+                break  # Stop after the first batch
+
+            B, _, H, W, D = image.shape
+            N = model.module.patch_num
+            PH, PW, PD = model.module.patch_dim
+            K = model.module.proto_num
+
+            # Keep only modality 0
+            image = image[:,0:1,:,:,:]
+
+            x = image.float()
+
+            if not args.cpu:
+                x = x.cuda()
+                gt = gt.cuda()
+
+            # Get embeddings and predictions
+            _, pred = model(x)
+
+            # Convert to probabilities
+            pred = pred.softmax(2)
+
+            # Convert prediction to (soft) cluster masks (restore spatial position of pooled image)
+            NPH = H//PH  # Num patches at X
+            NPW = W//PW  # Num patches at Y
+            NPD = D//PD  # Num patches at Z
+            pred = pred.permute(0,2,1).reshape((B,K,NPH,NPW,NPD))
+
+            # Plot prediction grid on tensorboard
+            n_epochs = min(10,args.epochs+1) # The number of epochs to sample from for the grid (10 or all epochs if total less than 10) (+1 because we always run for one extra epoch)
+            n_images = min(10,args.b)  # The number of images to sample from for the grid (10 or all images if total less than 10)
+            if args.vis and epoch % (args.epochs + 1 // n_epochs) == 0: 
+               
+                # If epoch 0, then also add the input images as the first row of the grid
+                if epoch == 0:  
+                    for img_idx in range(n_images):
+                        x_i = x[img_idx,0,:,:,D//2]                   
+                        x_i = (x_i - x_i.min())/(x_i.max() - x_i.min())  # Min-max norm input images
+                        x_i = x_i.repeat((3,1,1)).permute(1,2,0)  # Convert to RGB and move channel dim to the end
+                        x_i = x_i.cpu().detach()
+                        grid_preds.append(x_i)
+                
+                 # Next, add the predictions for each image at the current epoch
+                for img_idx in range(n_images): 
+                    pred_i = pred[img_idx,:,:,:,NPD//2].argmax(dim=0).unsqueeze(0)  # Take only hard cluster assignment (argmax)
+                    pred_i = f.interpolate(pred_i.float().unsqueeze(0), size=(H,W)).squeeze(0)  # Interpolate cluster masks to original input shape
+                    pred_i = pred_i.repeat((3,1,1)).permute(1,2,0)  # Convert to RGB and move channel dim to the end
+                    for i in range(colors.shape[0]):  # Give color to each cluster in cluster masks
+                        pred_i[pred_i[:,:,0] == i] = colors[i]
+                    pred_i = pred_i.cpu().detach()
+                    grid_preds.append(pred_i)
+
+    return grid_preds
 
 
 def train_cluster_3d(args, data_loader, run_dir, out_channel=3, writer=None):
